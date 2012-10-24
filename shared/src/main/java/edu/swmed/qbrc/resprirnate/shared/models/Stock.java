a69ec574f75f18c2eb2ff2f19a50955f0bf937f0
Jeff Allen <jeff.allen@trestletechnology.net>
2012-10-24 14:31:54 -0500

    Removed unused dependencies.

--- shared/src/main/java/edu/swmed/qbrc/resprirnate/shared/models/Stock.java
+++ shared/src/main/java/edu/swmed/qbrc/resprirnate/shared/models/Stock.java
@@ -1,13 +1,13 @@
 package edu.swmed.qbrc.resprirnate.shared.models;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
