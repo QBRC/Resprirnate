f9d36041fd2c54a65a38ac197dc9e64c98c020a1
trestletech <jeff.allen@trestletechnology.net>
2012-10-24 10:35:40 -0500

    Cleaned up imports.

--- src/main/java/com/mkyong/rest/MessageRestService.java
+++ src/main/java/com/mkyong/rest/MessageRestService.java
@@ -4,9 +4,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-
-import org.jboss.resteasy.annotations.providers.jaxb.json.BadgerFish;
 
 import com.mkyong.context.SpringApplicationContext;
 import com.mkyong.stock.bo.StockBo;
