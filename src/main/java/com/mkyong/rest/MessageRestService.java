bf5a4d6273023a1d1b00b4ef1a5d05726ef709cc
trestletech <jeff.allen@trestletechnology.net>
2012-10-24 08:43:10 -0500

    Corrected Hibernate dependency, Successfully create a stockBo object when fielding REST request.

--- src/main/java/com/mkyong/rest/MessageRestService.java
+++ src/main/java/com/mkyong/rest/MessageRestService.java
@@ -4,14 +4,23 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mkyong.stock.bo.StockBo;
  
-@Path("/message")
+@Path("/stock")
 public class MessageRestService {
  
 	@GET
 	@Path("/{param}")
 	public Response printMessage(@PathParam("param") String msg) {
- 
+		ApplicationContext appContext = 
+	    		new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
+		
+    	StockBo stockBo = (StockBo)appContext.getBean("stockBo");
+		
 		String result = "Restful example : " + msg;
  
 		return Response.status(200).entity(result).build();
