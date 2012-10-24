fb8794d947a71c72b213985c64792363c5546c88
Jeff Allen <jeff.allen@trestletechnology.net>
2012-10-24 15:44:20 -0500

    Updated client URL to properly work with Tomcat. Create messageRestService from Spring context before executing call.

--- client/src/main/java/edu/swmed/qbrc/resprirnate/app/App.java
+++ client/src/main/java/edu/swmed/qbrc/resprirnate/app/App.java
@@ -1,6 +1,5 @@
 package edu.swmed.qbrc.resprirnate.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,14 +7,15 @@ import edu.swmed.qbrc.resprirnate.shared.models.Stock;
 import edu.swmed.qbrc.resprirnate.shared.rest.MessageRestService;
 
 public class App {
-
-	@Autowired
+	
 	private static MessageRestService messageRestService;
 	
 	public static void main(String[] args) {
 		
 		ApplicationContext appContext =
 			     new ClassPathXmlApplicationContext("spring/config/applicationContext.xml");		
+		
+		messageRestService = (MessageRestService)appContext.getBean("stockClient");
 		
 		Stock stock = messageRestService.printMessage("7668");
 		System.out.println(stock);
